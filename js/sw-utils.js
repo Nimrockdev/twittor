0e9bec61d08a460bf25bfedd964d4f534d14e3fa
Javier <jsg8405@gmail.com>
2019-10-29 06:02:23 +0100

    Iniciamos repositorio

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+//Guarda en el cahce dinamico
+function actualizaCacheDinamico(dynamicCache, req, res) {
+
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        })
+    } else {
+        return res;
+    }
+
+
+}
