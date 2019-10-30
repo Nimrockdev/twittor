116cd37ac576a29093faa93b00c785c1af6211ea
Javier <jsg8405@gmail.com>
2019-10-30 06:59:25 +0100

    IOS notch

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js')
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const IMMUTABLE_CACHE = 'inmutable-v1';
 
 //app_shell todo lo necesario para que funcione la aplicación
@@ -45,6 +45,9 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
 
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         });
